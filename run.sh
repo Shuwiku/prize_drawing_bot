6105f289670e019672dbd26635cb0675c3a48be1
Shuwiku <shuwiku@gmail.com>
2024-11-18 11:46:42 +0700

    Небольшое улучшение

--- run.sh
+++ run.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
-python3 -m venv venv
-source ./venv/bin/activate
-python3 -m pip install -r ./requirements.txt
-clear
+
+quiet() {
+    "$@" > /dev/null 2>&1
+}
+
+dirScript=$PWD
+dirVenv="$PWD/venv"
+fileRequirements="$PWD/requirements.txt"
+
+echo "Создаю виртуальное окружение по адресу: '$dirVenv'."
+quiet python3 -m venv venv
+
+echo "Запускаю виртуальное окружение."
+quiet source ./venv/bin/activate
+
+echo "Устанавливаю зависимости из файла: '$fileRequirements'."
+quiet python3 -m pip install -r ./requirements.txt
+
+echo "Запускаю бота."
 python3 ./prize_drawing_bot\
+
+pause
