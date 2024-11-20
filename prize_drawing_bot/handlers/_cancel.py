f129abf422037e3728728b0187302e6ec322251e
Олег <shuwiku@gmail.com>
2024-11-21 00:15:10 +0700

    Обработчик cancel для отключения машины состояний.

---
+++ prize_drawing_bot/handlers/_cancel.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""Обработчики команды/запроса, останавливающих машину состояний."""
+
+from typing import Final
+
+from aiogram import F, Router
+from aiogram.filters import Command
+from aiogram.types import CallbackQuery, Message
+from aiogram.fsm.context import FSMContext
+from aiogram_i18n.context import I18nContext
+
+from filters import CallbackMessageFromUser, MessageFromUser
+from utils import change_keyboard
+
+
+router: Final[Router] = Router(name=__name__)
+
+
+@router.message(Command("cancel"),
+                MessageFromUser())
+async def cmd_cancel(message: Message,
+                     i18n: I18nContext,
+                     state: FSMContext
+                     ) -> None:
+    """Останавливает машину состояний."""
+    if await state.get_state():  # Машина состояний запущена
+        await state.clear()
+        await change_keyboard(chat_id=message.from_user.id,  # type: ignore
+                              message_id=message.message_id - 1,
+                              reply_markup=None)
+        await message.answer(text=i18n.get("action-canceled"))
+
+
+@router.callback_query(F.data == "cancel")
+@router.message(CallbackMessageFromUser())
+async def cal_cancel(callback: CallbackQuery,
+                     i18n: I18nContext,
+                     state: FSMContext
+                     ) -> None:
+    """Останавливает машину состояний.
+
+    Останавливает машину состояний и убирает inline-клавиатуру.
+    """
+    if not await state.get_state():  # Машина состояний не запущена
+        return
+
+    message: Message = callback.message  # type: ignore
+
+    await message.edit_reply_markup(reply_markup=None)
+    await message.answer(text=i18n.get("action-canceled"))
+
+    await state.clear()
+    await callback.answer(show_alert=False)
