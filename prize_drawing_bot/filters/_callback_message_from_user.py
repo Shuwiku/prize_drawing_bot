1007b10320d295b26faeeb6ed6fc9c52110cad11
Олег <shuwiku@gmail.com>
2024-11-21 00:20:17 +0700

    Фильтр CallbackMessageFromUser, который имеет схожий функционал с MessageFromUser

---
+++ prize_drawing_bot/filters/_callback_message_from_user.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""Фильтр, проверяющий доступ к параметру callback.message.
+
+В некоторых случаях этот параметр может отсутствовать (быть None), или быть
+типа не Message, а InaccessibleMessage, например, если сообщение было удалено.
+""" 
+
+from aiogram.filters import Filter
+from aiogram.types import Message, CallbackQuery
+from aiogram_i18n.context import I18nContext
+
+
+class CallbackMessageFromUser(Filter):
+    """Фильтр, проверяющий доступ к параметру callback.message."""
+
+    async def __call__(self,
+                       callback: CallbackQuery,
+                       i18n: I18nContext
+                       ) -> bool:
+        """Проверяет доступ к параметру callback.message.
+
+        Returns:
+            bool: True, если есть доступ к параметру callback.message.
+                В противном случае - False.
+        """
+        if type(callback.message) == Message:
+            return True
+        await callback.answer(text=i18n.get("callback-not-from-user"))
+        return False
