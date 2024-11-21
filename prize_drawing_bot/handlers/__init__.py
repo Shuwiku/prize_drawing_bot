1b059a3a6bd91906e065233ab5d13ed6a82fa128
Олег <shuwiku@gmail.com>
2024-11-21 23:45:14 +0700

    Немного изменил испорт для более простой настройки используемых обработчиков. В будущем будет ещё одно-пара изменений.

--- prize_drawing_bot/handlers/__init__.py
+++ prize_drawing_bot/handlers/__init__.py
@@ -1,23 +1,25 @@
 # -*- coding: utf-8 -*-
 """Обработчики событий бота."""
 
-from typing import Final, List
+import importlib
+from typing import List
 
 from aiogram import Router
 
-from . import (
-    _cancel,
-    _language,
-    _register,
-    _start
-)
 
-
-__routers: Final[List[Router]] = [
-    _cancel.router,
-    _language.router,
-    _register.router,
-    _start.router
+# Это пока что первый вариант. В будущем это будет список в файле конфигурации
+# бота. Это позволит чуть упростить настройку используемых обработчиков.
+# Ещё надо что-то придумать с припиской "handler._". Посмотрим.
+_handlers: dict = {}
+_required_handlers = [
+    "handlers._cancel",
+    "handlers._register",
+    "handlers._start"
 ]
+_routers: List[Router] = []
+for i in _required_handlers:
+    _handlers[i] = importlib.import_module(i)
+    _routers.append(getattr(_handlers[i], "router"))
+
 router_handlers: Router = Router(name="router_handlers")
-router_handlers.include_routers(*__routers)
+router_handlers.include_routers(*_routers)
